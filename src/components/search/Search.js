b31acb48d44ad0e3f1552230e80e893e8845f7c3
Denis Bondarenko <denbon04@gmail.com>
2020-12-24 18:43:45 +0200

    start making search component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,17 +2,38 @@ import React from 'react'
 import search from '../../images/search_icon.svg'
 import './Search.css'
 
-function Search() {
-    return (
-        <div>
-            <div className='search'>
-                <button className='search_btn'>
-                    <img className='search_icon' src={search} alt='seacch_icon' />
-                </button>
-                <input className='search_input' placeholder='Search a stock' />
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            company: ''
+        }
+    }
+
+    searchEditText;
+
+    componentDidMount() {
+        this.searchEditText = document.getElementById('searchInp');
+    }
+
+    onSearchButtonClick = () => {
+        this.setState({company: this.searchEditText.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='first_row'>
+                    <div className='search'>
+                        <button className='search_btn' onClick={this.onSearchButtonClick}>
+                            <img className='search_icon' src={search} alt='seacch_icon' />
+                        </button>
+                        <input id='searchInp' className='search_input' placeholder='Search a stock' />
+                    </div>
+                    <p className='search_hint'>You have to write company's NASDAQ</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Search
